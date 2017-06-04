7dde27f9c2ec34e64d91e60c338079c9bafd9c4d
Alexander Lobov <Aleksandr.Lobov@t-systems.ru>
2017-06-04 18:07:39 +0300

    My first bundle

---
+++ app/sub-component/sub-component.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector:'sub-component',
+    templateUrl:'./sub-component.component.html',
+    styleUrls: ['./sub-component.component.css']
+
+})
+export class SubComponent{
+
+}
