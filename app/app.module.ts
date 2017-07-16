660cf73255560530aee97cbaaeb8416a2fddabd4
Your Name <you@example.com>
2017-07-16 03:15:08 +0300

    modules and routes have been added

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,13 +1,19 @@
 import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { SubComponent } from './sub-component/sub-component.component';
+import { OtherComponentModule } from './other-component/other-component.module';
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './app.routes';
+import { RootComponent } from './root.component';
 
 @NgModule({
-  imports: [BrowserModule],
-  declarations: [AppComponent, SubComponent],
-  bootstrap: [AppComponent]
+  imports: [
+    BrowserModule,
+    OtherComponentModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  declarations: [AppComponent, RootComponent],
+  bootstrap: [RootComponent]
 })
 export class AppModule {
 }
-
---
+++ app/other-component/other-component.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { OtherComponent } from './other-component.component';
+
+@NgModule({
+    declarations: [
+      OtherComponent
+    ]
+  }
+)
+export class OtherComponentModule {
+}
