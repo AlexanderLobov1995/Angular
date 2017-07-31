b6b35d4fcc3cb39044788e2216b348bc40490540
Your Name <you@example.com>
2017-07-31 10:30:54 +0300

    Adjusting some imports

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,5 @@
 import { AppModule } from './app.module';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import 'rxjs/add/operator/map';
+import 'rxjs/Rx';
 
 platformBrowserDynamic().bootstrapModule(AppModule);
--- app/vendor.ts
+++ app/vendor.ts
@@ -6,6 +6,8 @@ import '@angular/common';
 import '@angular/http';
 import '@angular/router';
 
+import 'rxjs/Rx';
+
 import 'core-js/es6';
 import 'core-js/es7/reflect';
 import 'zone.js';
